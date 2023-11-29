2b8e0a54de7bd941e84444452e158d2c57e920f0
Alex Embrey <alex.embrey@apexrms.com>
2023-11-29 14:31:26 -0800

    Initial commit of model linking demo package

---
+++ src/Main.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Data;
+using SyncroSim.Core;
+
+namespace LinkModels
+{
+    public class Main : Transformer
+    {
+        public override void Transform()
+        {
+            DataRow dr1 = this.ResultScenario.GetDataSheet("dgsim_RunControl").GetDataRow();
+            DataRow dr2 = this.ResultScenario.GetDataSheet("demosales_RunControl").GetDataRow();
+
+            dr1["MinimumIteration"] = dr2["MinimumIteration"];
+            dr1["MaximumIteration"] = dr2["MaximumIteration"];
+            dr1["MinimumTimestep"] = dr2["MinimumTimestep"];
+            dr1["MaximumTimestep"] = dr2["MaximumTimestep"];
+        }
+    }
+}
